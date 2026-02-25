b2c99d82b632ed2847f92980100f087147159cd7
Mati <Mati@Mati>
2026-02-25 19:25:00 +0100

    run: El saldo actual es: 2500.0 Ingreso en cuenta BUILD SUCCESSFUL (total time: 0 seconds)

---
+++ src/cuentas/Main.java
@@ -0,0 +1,29 @@
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(17);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es: "+ saldoActual ); //modificado para añadir un espacio después del string y así no tener "es2500.0"
+
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
